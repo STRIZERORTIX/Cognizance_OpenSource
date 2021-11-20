d85bebea4d610e36dc281e6f7e94051e989436ab
ABHINANDHAN S <abhi.varadan@gmail.com>
2021-11-20 23:48:29 +0530

    Completed TO-Do

--- Task 2/app.js
+++
@@ -1,6 +0,0 @@
-let progress = document.getElementById('pbar');
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function(){
-	let progressHeight = (window.pageYOffset / totalHeight) * 100;
-	progress.style.height = progressHeight + "%";
-}
